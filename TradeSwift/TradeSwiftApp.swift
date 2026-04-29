d4cc469ddeb294c9899be5cd1d57b9b60c24f566
BT_C09_08 <mobileappdev@bths.edu>
2026-04-29 13:36:57 -0400

    Initial Commit

---
+++ TradeSwift/TradeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeSwiftApp.swift
+//  TradeSwift
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TradeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
