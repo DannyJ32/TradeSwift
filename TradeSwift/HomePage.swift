fec2d127023c0d32d8adf969a505f68d36c3aa16
BT_C09_03 <mobileappdev@bths.edu>
2026-05-04 13:42:21 -0400

    Basic GUI

--- TradeSwift/HomePage.swift
+++ TradeSwift/HomePage.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        Text("Home Page")
+        VStack {
+            HStack {
+                Text("Hello")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.GREEN_1)
+            HStack {
+                Text("Test")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.GREEN_2)
+            HStack {
+                Text("Last")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.GREEN_3)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
